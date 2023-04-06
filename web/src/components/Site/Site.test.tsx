52c6204f2ebf4321c9a8cdf3c9dccde39ef4e9d0
James Stevenson <jstevenson@ravenpack.com>
2023-04-06 17:52:21 +0200

    Fix tests

--- web/src/components/Site/Site.test.tsx
+++ web/src/components/Site/Site.test.tsx
@@ -8,7 +8,17 @@ import Site from "./Site"
 describe("Site", () => {
   it("renders successfully", () => {
     expect(() => {
-      render(<Site />)
+      render(
+        <Site
+          site={{
+            id: 1,
+            name: "string",
+            description: "string",
+            address: "string",
+            createdAt: "2021-05-05T20:00:00.000Z",
+          }}
+        />,
+      )
     }).not.toThrow()
   })
 })
