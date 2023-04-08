d6b7f19f798a07c4d922e76dfa76e3e7dacef1ae
James Stevenson <jstevenson@ravenpack.com>
2023-04-08 10:44:51 +0200

    Admin Layout

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,12 +7,9 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set, Private } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from "@redwoodjs/router"
 
-import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
-
-import ScaffoldLayout from "src/layouts/ScaffoldLayout"
-
+import AdminLayout from "src/layouts/AdminLayout"
 import MainLayout from "src/layouts/MainLayout"
 
 import { useAuth } from "./auth"
@@ -20,43 +17,43 @@ import { useAuth } from "./auth"
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Set wrap={ScaffoldLayout} title="Payments" titleTo="payments" buttonLabel="New Payment" buttonTo="newPayment">
+      <Set wrap={AdminLayout} title="Payments" titleTo="payments" buttonLabel="New Payment" buttonTo="newPayment">
         <Route path="/payments/new" page={PaymentNewPaymentPage} name="newPayment" />
         <Route path="/payments/{id:Int}/edit" page={PaymentEditPaymentPage} name="editPayment" />
         <Route path="/payments/{id:Int}" page={PaymentPaymentPage} name="payment" />
         <Route path="/payments" page={PaymentPaymentsPage} name="payments" />
       </Set>
-      <Set wrap={ScaffoldLayout} title="Contacts" titleTo="contacts" buttonLabel="New Contact" buttonTo="newContact">
+      <Set wrap={AdminLayout} title="Contacts" titleTo="contacts" buttonLabel="New Contact" buttonTo="newContact">
         <Route path="/contacts/new" page={ContactNewContactPage} name="newContact" />
         <Route path="/contacts/{id:Int}/edit" page={ContactEditContactPage} name="editContact" />
         <Route path="/contacts/{id:Int}" page={ContactContactPage} name="contact" />
         <Route path="/contacts" page={ContactContactsPage} name="contacts" />
       </Set>
-      <Set wrap={ScaffoldLayout} title="Companies" titleTo="companies" buttonLabel="New Company" buttonTo="newCompany">
+      <Set wrap={AdminLayout} title="Companies" titleTo="companies" buttonLabel="New Company" buttonTo="newCompany">
         <Route path="/companies/new" page={CompanyNewCompanyPage} name="newCompany" />
         <Route path="/companies/{id:Int}/edit" page={CompanyEditCompanyPage} name="editCompany" />
         <Route path="/companies/{id:Int}" page={CompanyCompanyPage} name="company" />
         <Route path="/companies" page={CompanyCompaniesPage} name="companies" />
       </Set>
-      <Set wrap={ScaffoldLayout} title="Jobs" titleTo="jobs" buttonLabel="New Job" buttonTo="newJob">
+      <Set wrap={AdminLayout} title="Jobs" titleTo="jobs" buttonLabel="New Job" buttonTo="newJob">
         <Route path="/jobs/new" page={JobNewJobPage} name="newJob" />
         <Route path="/jobs/{id:Int}/edit" page={JobEditJobPage} name="editJob" />
         <Route path="/jobs/{id:Int}" page={JobJobPage} name="job" />
         <Route path="/jobs" page={JobJobsPage} name="jobs" />
       </Set>
-      <Set wrap={ScaffoldLayout} title="Contracts" titleTo="contracts" buttonLabel="New Contract" buttonTo="newContract">
+      <Set wrap={AdminLayout} title="Contracts" titleTo="contracts" buttonLabel="New Contract" buttonTo="newContract">
         <Route path="/contracts/new" page={ContractNewContractPage} name="newContract" />
         <Route path="/contracts/{id:Int}/edit" page={ContractEditContractPage} name="editContract" />
         <Route path="/contracts/{id:Int}" page={ContractContractPage} name="contract" />
         <Route path="/contracts" page={ContractContractsPage} name="contracts" />
       </Set>
-      <Set wrap={ScaffoldLayout} title="Categories" titleTo="categories" buttonLabel="New Category" buttonTo="newCategory">
+      <Set wrap={AdminLayout} title="Categories" titleTo="categories" buttonLabel="New Category" buttonTo="newCategory">
         <Route path="/categories/new" page={CategoryNewCategoryPage} name="newCategory" />
         <Route path="/categories/{id:Int}/edit" page={CategoryEditCategoryPage} name="editCategory" />
         <Route path="/categories/{id:Int}" page={CategoryCategoryPage} name="category" />
         <Route path="/categories" page={CategoryCategoriesPage} name="categories" />
       </Set>
-      <Set wrap={ScaffoldLayout} title="Buildings" titleTo="buildings" buttonLabel="New Building" buttonTo="newBuilding">
+      <Set wrap={AdminLayout} title="Buildings" titleTo="buildings" buttonLabel="New Building" buttonTo="newBuilding">
         <Route path="/buildings/new" page={BuildingNewBuildingPage} name="newBuilding" />
         <Route path="/buildings/{id:Int}/edit" page={BuildingEditBuildingPage} name="editBuilding" />
         <Route path="/buildings/{id:Int}" page={BuildingBuildingPage} name="building" />
@@ -76,46 +73,46 @@ const Routes = () => {
       </Set>
       {/* Admin */}
       <Private unauthenticated="home">
-        <Set wrap={ScaffoldLayout} title="Admin" titleTo="admin">
+        <Set wrap={AdminLayout} title="Admin" titleTo="admin">
           <Route path="/admin" page={AdminPage} name="admin" />
         </Set>
-        <Set wrap={ScaffoldLayout} title="Payments" titleTo="payments" buttonLabel="New Payment" buttonTo="newPayment">
+        <Set wrap={AdminLayout} title="Payments" titleTo="payments" buttonLabel="New Payment" buttonTo="newPayment">
           <Route path="/admin/payments/new" page={PaymentNewPaymentPage} name="newPayment" />
           <Route path="/admin/payments/{id:Int}/edit" page={PaymentEditPaymentPage} name="editPayment" />
           <Route path="/admin/payments/{id:Int}" page={PaymentPaymentPage} name="payment" />
           <Route path="/admin/payments" page={PaymentPaymentsPage} name="payments" />
         </Set>
-        <Set wrap={ScaffoldLayout} title="Contacts" titleTo="contacts" buttonLabel="New Contact" buttonTo="newContact">
+        <Set wrap={AdminLayout} title="Contacts" titleTo="contacts" buttonLabel="New Contact" buttonTo="newContact">
           <Route path="/admin/contacts/new" page={ContactNewContactPage} name="newContact" />
           <Route path="/admin/contacts/{id:Int}/edit" page={ContactEditContactPage} name="editContact" />
           <Route path="/admin/contacts/{id:Int}" page={ContactContactPage} name="contact" />
           <Route path="/admin/contacts" page={ContactContactsPage} name="contacts" />
         </Set>
-        <Set wrap={ScaffoldLayout} title="Companies" titleTo="companies" buttonLabel="New Company" buttonTo="newCompany">
+        <Set wrap={AdminLayout} title="Companies" titleTo="companies" buttonLabel="New Company" buttonTo="newCompany">
           <Route path="/admin/companies/new" page={CompanyNewCompanyPage} name="newCompany" />
           <Route path="/admin/companies/{id:Int}/edit" page={CompanyEditCompanyPage} name="editCompany" />
           <Route path="/admin/companies/{id:Int}" page={CompanyCompanyPage} name="company" />
           <Route path="/admin/companies" page={CompanyCompaniesPage} name="companies" />
         </Set>
-        <Set wrap={ScaffoldLayout} title="Jobs" titleTo="jobs" buttonLabel="New Job" buttonTo="newJob">
+        <Set wrap={AdminLayout} title="Jobs" titleTo="jobs" buttonLabel="New Job" buttonTo="newJob">
           <Route path="/admin/jobs/new" page={JobNewJobPage} name="newJob" />
           <Route path="/admin/jobs/{id:Int}/edit" page={JobEditJobPage} name="editJob" />
           <Route path="/admin/jobs/{id:Int}" page={JobJobPage} name="job" />
           <Route path="/admin/jobs" page={JobJobsPage} name="jobs" />
         </Set>
-        <Set wrap={ScaffoldLayout} title="Contracts" titleTo="contracts" buttonLabel="New Contract" buttonTo="newContract">
+        <Set wrap={AdminLayout} title="Contracts" titleTo="contracts" buttonLabel="New Contract" buttonTo="newContract">
           <Route path="/admin/contracts/new" page={ContractNewContractPage} name="newContract" />
           <Route path="/admin/contracts/{id:Int}/edit" page={ContractEditContractPage} name="editContract" />
           <Route path="/admin/contracts/{id:Int}" page={ContractContractPage} name="contract" />
           <Route path="/admin/contracts" page={ContractContractsPage} name="contracts" />
         </Set>
-        <Set wrap={ScaffoldLayout} title="Categories" titleTo="categories" buttonLabel="New Category" buttonTo="newCategory">
+        <Set wrap={AdminLayout} title="Categories" titleTo="categories" buttonLabel="New Category" buttonTo="newCategory">
           <Route path="/admin/categories/new" page={CategoryNewCategoryPage} name="newCategory" />
           <Route path="/admin/categories/{id:Int}/edit" page={CategoryEditCategoryPage} name="editCategory" />
           <Route path="/admin/categories/{id:Int}" page={CategoryCategoryPage} name="category" />
           <Route path="/admin/categories" page={CategoryCategoriesPage} name="categories" />
         </Set>
-        <Set wrap={ScaffoldLayout} title="Buildings" titleTo="buildings" buttonLabel="New Building" buttonTo="newBuilding">
+        <Set wrap={AdminLayout} title="Buildings" titleTo="buildings" buttonLabel="New Building" buttonTo="newBuilding">
           <Route path="/admin/buildings/new" page={BuildingNewBuildingPage} name="newBuilding" />
           <Route path="/admin/buildings/{id:Int}/edit" page={BuildingEditBuildingPage} name="editBuilding" />
           <Route path="/admin/buildings/{id:Int}" page={BuildingBuildingPage} name="building" />
---
+++ web/src/layouts/AdminLayout/AdminLayout.tsx
@@ -0,0 +1,89 @@
+import { Link, routes } from "@redwoodjs/router"
+import { Toaster } from "@redwoodjs/web/toast"
+
+type LayoutProps = {
+  title: string
+  titleTo: string
+  buttonLabel?: string
+  buttonTo?: string
+  children: React.ReactNode
+}
+
+const AdminLayout = ({ buttonLabel, buttonTo, children }: LayoutProps) => {
+  const navigation = [
+    {
+      href: routes.admin(),
+      name: "Admin",
+    },
+    {
+      href: routes.buildings(),
+      name: "Buildings",
+    },
+    {
+      href: routes.contracts(),
+      name: "Contracts",
+    },
+    {
+      href: routes.jobs(),
+      name: "Jobs",
+    },
+    {
+      href: routes.companies(),
+      name: "Companies",
+    },
+    {
+      href: routes.contacts(),
+      name: "Contacts",
+    },
+    {
+      href: routes.payments(),
+      name: "Payments",
+    },
+    {
+      href: routes.categories(),
+      name: "Categories",
+    },
+  ]
+
+  return (
+    <>
+      <div className="mx-auto max-w-screen-xl px-4 pt-4 md:px-8">
+        <Toaster toastOptions={{ className: "rw-toast", duration: 6000 }} />
+        <div className="flex justify-between">
+          <h3 className="text-2xl font-bold text-gray-800">
+            <Link to={routes.home()}>LMS</Link>
+          </h3>
+          {buttonLabel && buttonTo && (
+            <Link to={routes[buttonTo]()} className="rw-button rw-button-green">
+              <div className="rw-button-icon">+</div> {buttonLabel}
+            </Link>
+          )}
+        </div>
+        <div className="mb-8 mt-6">
+          <ul className="flex w-full items-center gap-x-3 overflow-x-auto border-b">
+            {navigation.map((item, idx) => (
+              <li
+                key={idx}
+                className={`border-b-2 py-2 ${
+                  window.location.pathname === item.href
+                    ? "border-purple-600 text-purple-600"
+                    : "border-white text-gray-500"
+                }`}
+              >
+                <a
+                  href={item.href}
+                  className="rounded-lg px-4 py-2.5 text-sm font-medium duration-150 hover:bg-gray-50 hover:text-purple-600 active:bg-gray-100"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <main className="rw-main">{children}</main>
+      </div>
+    </>
+  )
+}
+
+export default AdminLayout
